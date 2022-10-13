0f005e5d27941abf99d63693eff99151f8810aeb
denisamihut <denisa.m_19@yahoo.com>
2022-10-13 12:33:35 +0300

    updating project

---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  
+  $("#flag1").mouseover(function(){
+     $(".banner").text("Officially the Hellenic Republic,[b] is a country in Southeast Europe. It is situated on the southern tip of the Balkans, and is located at the crossroads of Europe, Asia, and Africa. Greece shares land borders with Albania to the northwest, North Macedonia and Bulgaria to the north, and Turkey to the northeast. The Aegean Sea lies to the east of the mainland, the Ionian Sea to the west, and the Sea of Crete and the Mediterranean Sea to the south. Greece has the longest coastline on the Mediterranean Basin, featuring thousands of islands. The country consists of nine traditional geographic regions, and has a population of approximately 10.4 million. Athens is the nation's capital and largest city, followed by Thessaloniki and Patras.");
+   });
+  
+   $("#flag2").mouseover(function(){
+     $(".banner").text("Officially the Slovak Republic (Slovak: Slovenská republika [ˈslɔʋenskaː ˈrepublika] (listen)), is a landlocked country in Central Europe. It is bordered by Poland to the north, Ukraine to the east, Hungary to the south, Austria to the southwest, and the Czech Republic to the northwest. Slovakia's mostly mountainous territory spans about 49,000 square kilometres (19,000 sq mi), with a population of over 5.4 million. The capital and largest city is Bratislava, while the second largest city is Košice.");
+   });
+  
+   $("#flag3").mouseover(function(){
+     $(".banner").text(" Is a country located at the crossroads of Central, Eastern, and Southeastern Europe. It borders Bulgaria to the south, Ukraine to the north, Hungary to the west, Serbia to the southwest, Moldova to the east, and the Black Sea to the southeast. It has a predominantly temperate-continental climate, and an area of 238,397 km2 (92,046 sq mi), with a population of around 19 million. Romania is the twelfth-largest country in Europe and the sixth-most populous member state of the European Union. Its capital and largest city is Bucharest, followed by Iași, Cluj-Napoca, Timișoara, Constanța, Craiova, Sibiu, Brașov, and Galați.");
+   });
+  
+});
